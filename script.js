ef102b6264894c77653b76276cf7b1a0039febdc
Ayush Tandon <121681555+ayushtan123@users.noreply.github.com>
2024-09-06 19:03:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ async function displayAlbums() {
     //load the playlist whenever the card is clicked
     Array.from(document.getElementsByClassName("card")).forEach(e => {
         e.addEventListener("click", async item => {
-            songs = await getSongs(`https://ayushtan123.github.io/Tunify/songs/${item.currentTarget.dataset.folder}`)
+            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`)
             console.log(songs);
             playMusic(songs[0])
 
